4b84125586b572ee071cd9fdac2f0a69859e64ca
NataGrigoreva <gr.natalij@gmail.com>
2024-03-27 17:57:45 +0300

    6_02 / 2 дз AbstractMealServiceTest + pom

--- src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
@@ -13,6 +13,7 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringRunner;
 import ru.javawebinar.topjava.ActiveDbProfileResolver;
+import ru.javawebinar.topjava.TimingRules;
 
 //import java.util.concurrent.TimeUnit;
 import java.util.concurrent.*;
@@ -33,25 +34,26 @@ public abstract class AbstractServiceTest {
 
     @Rule
     // http://stackoverflow.com/questions/14892125/what-is-the-best-practice-to-determine-the-execution-time-of-the-bussiness-relev
-    public Stopwatch stopwatch = new Stopwatch() {
-        @Override
-        protected void finished(long nanos, Description description) {
-//            String result = String.format("%-95s %7d", description.getDisplayName(), TimeUnit.NANOSECONDS.toMillis(nanos));
-            String result = String.format("%-95s %7d", description.getDisplayName(), (nanos / 1_000_000));
-            results.append(result).append('\n');
-            log.info(result + " ms\n");
-        }
-    };
-
-
-    //    https://dzone.com/articles/applying-new-jdk-11-string-methods
-    private static final String DELIM = "-".repeat(103);
-
-    @AfterClass
-    public static void printResult() {
-        log.info("\n" + DELIM +
-                "\nTest                                                                                       Duration, ms" +
-                "\n" + DELIM + "\n" + results + DELIM + "\n");
-        results.setLength(0);
-    }
+//    public Stopwatch stopwatch = new Stopwatch() {
+//        @Override
+//        protected void finished(long nanos, Description description) {
+////            String result = String.format("%-95s %7d", description.getDisplayName(), TimeUnit.NANOSECONDS.toMillis(nanos));
+//            String result = String.format("%-95s %7d", description.getDisplayName(), (nanos / 1_000_000));
+//            results.append(result).append('\n');
+//            log.info(result + " ms\n");
+//        }
+//    };
+//
+//
+//    //    https://dzone.com/articles/applying-new-jdk-11-string-methods
+//    private static final String DELIM = "-".repeat(103);
+//
+//    @AfterClass
+//    public static void printResult() {
+//        log.info("\n" + DELIM +
+//                "\nTest                                                                                       Duration, ms" +
+//                "\n" + DELIM + "\n" + results + DELIM + "\n");
+//        results.setLength(0);
+//    }
+    public Stopwatch stopwatch = TimingRules.STOPWATCH;
 }
