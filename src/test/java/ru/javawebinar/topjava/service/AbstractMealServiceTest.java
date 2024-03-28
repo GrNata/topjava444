47c398dd7e5fa5cdfba32b55b065f87f679352c2
NataGrigoreva <gr.natalij@gmail.com>
2024-03-28 11:20:13 +0300

    6_06 optional fetch join

--- src/test/java/ru/javawebinar/topjava/service/AbstractMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractMealServiceTest.java
@@ -50,7 +50,7 @@ public abstract class AbstractMealServiceTest extends AbstractServiceTest {
 //    };
 
     @Autowired
-    private MealService service;
+    protected MealService service;
 
     @Test
     public void delete() {
