30c1a44dffc9d694ee3b5da7dd3ebbc10ed0c05a
NataGrigoreva <gr.natalij@gmail.com>
2024-04-04 13:23:25 +0300

    дз optional 1 jdbc

--- src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcUserServiceTest.java
@@ -6,7 +6,7 @@ import ru.javawebinar.topjava.service.AbstractUserServiceTest;
 
 import static ru.javawebinar.topjava.Profiles.JDBC;
 
-@Ignore
+//@Ignore
 @ActiveProfiles(JDBC)
 public class JdbcUserServiceTest extends AbstractUserServiceTest {
 }
