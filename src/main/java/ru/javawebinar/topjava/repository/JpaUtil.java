11235959bb41e88068425303656eeff43b47827e
NataGrigoreva <gr.natalij@gmail.com>
2024-04-05 07:47:57 +0300

    дз optional test cache

--- src/main/java/ru/javawebinar/topjava/repository/JpaUtil.java
+++ src/main/java/ru/javawebinar/topjava/repository/JpaUtil.java
@@ -6,16 +6,16 @@ import org.hibernate.SessionFactory;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-public class JpaUtil {
-
-    @PersistenceContext
-    private EntityManager em;
-
-    public void clear2ndLevelHibernateCache() {
-        Session s = (Session) em.getDelegate();
-        SessionFactory sf = s.getSessionFactory();
-//        sf.getCache().evictEntityData(User.class, AbstractBaseEntity.START_SEQ);
-//        sf.getCache().evictEntityData(User.class);
-        sf.getCache().evictAllRegions();
-    }
-}
+//public class JpaUtil {
+//
+//    @PersistenceContext
+//    private EntityManager em;
+//
+//    public void clear2ndLevelHibernateCache() {
+//        Session s = (Session) em.getDelegate();
+//        SessionFactory sf = s.getSessionFactory();
+////        sf.getCache().evictEntityData(User.class, AbstractBaseEntity.START_SEQ);
+////        sf.getCache().evictEntityData(User.class);
+//        sf.getCache().evictAllRegions();
+//    }
+//}
